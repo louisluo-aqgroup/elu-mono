4d422471f548b9e36a2bfb2ee10564ddffa279ff
Louis Luo <louisluo@aq.group>
2025-11-05 15:06:39 +0800

    Fix api rewrites

--- apps/elu-storybook/configs/rewrites.ts
+++ apps/elu-storybook/configs/rewrites.ts
@@ -1,14 +1,11 @@
 import envVariable from './env';
 
-const apiRewrites =
-  process.env.NODE_ENV === 'development'
-    ? [
-        {
-          source: '/api/:path*',
-          destination: `${envVariable.API_URL}/api/:path*`,
-        },
-      ]
-    : [];
+const apiRewrites = [
+  {
+    source: '/api/:path*',
+    destination: `${envVariable.API_URL}/api/:path*`,
+  },
+];
 
 const nextRewrites: import('next').NextConfig['rewrites'] = async () => {
   return [...apiRewrites];
